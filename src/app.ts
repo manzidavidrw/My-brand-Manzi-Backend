9dcb7bd3a05475c0109b4998d6407d068395fcfe
manzidavidrw <manzi2020d@gmail.com>
2024-02-18 16:34:14 +0200

    fixing likes

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import blogRoutes from './routes/blogRoutes';
 import commentRoutes from './routes/commentRoutes';
 import messageRoutes from './routes/messageRoutes';
+import likeRoutes from './routes/likeRoutes'
 import { createLike } from './controllers/likeController';
 
 const app = express();
@@ -20,7 +21,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true } as a
 app.use('/api/blogs', blogRoutes);
 app.use('/api/blogs', commentRoutes);
 app.use('/api/messages', messageRoutes);
-app.use('/api/blogs', createLike);
+app.use('/api/blogs', likeRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
