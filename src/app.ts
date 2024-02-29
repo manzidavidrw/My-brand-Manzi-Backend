22364b9c401c2d451ee33e706807000876a7ff6b
manzidavidrw <manzi2020d@gmail.com>
2024-02-29 17:00:41 +0200

    circle ci

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,7 @@ app.use('/swagger',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 
 
 const PORT = process.env.PORT || '';
+
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
