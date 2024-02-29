82861db3458967734339e3775bf69402e66be622
manzidavidrw <manzi2020d@gmail.com>
2024-02-29 11:50:57 +0200

    swagger

--- dist/routes/userroutes.js
+++ dist/routes/userroutes.js
@@ -29,6 +29,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const Users = __importStar(require("../controllers/usersController"));
-router.post("/signup", Users.createUser);
+ router.post("/signup", Users.createUser);
 router.post("/login", Users.loginUser);
 exports.default = router;
